b85e57f37569f74d82b5a4bd12deaf23ffbdf8c6
sathiya-narayanan <30971169+sathiya-narayanan@users.noreply.github.com>
2019-12-27 14:15:06 +0530

    Add files via upload

---
+++ Factors of a number.py
@@ -0,0 +1,9 @@
+print("enter a number")
+n=int(input())
+i=1
+print("the factors of the number is/are")
+for i in range(1,n+1):
+       if(int(n%i)==0):
+           print(i)
+            
+            
